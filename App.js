b9a49334eeb204d7a55aa727141010c4a87d6428
rnik22 <80130605+rnik22@users.noreply.github.com>
2022-11-21 23:49:41 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { useState, useRef } from 'react';
+import Dashboard from './Dashboard';
+import 'bootstrap/dist/css/bootstrap.css';
+import FormInput from './components/FormInput';
+
+function App() {
+
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App;
